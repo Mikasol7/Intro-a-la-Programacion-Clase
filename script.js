cc0ae703a25e25d7b9358f6881ff9ec2f3e02781
Mikasol7 <micaelasol-l@hotmail.com>
2025-10-01 23:49:29 +0000

    Clase 1ero oct

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
-const submit =document.getElementById("submit")
-console.log(submit.target)
-const input =document.getElementById("input")
-console.log(input.target)
-submit.onclick =(e) =>{
-    console.log(input.value)
-    e.preventDefault()
+const nombre= "Micaela"
+let edad= 28
+const saludo= `Hola, me llamo ${nombre}, tengo ${edad} años`;
+console.log(nombre)
+console.log(edad)
+console.log(saludo)
+console.log("1"==1)
+console.log("1"===1)
+const lista=[1,"hola",null]
+lista.length
+console.log(lista)
+function saludar (nombre) {
+    console.log(`Hola ${nombre}`)
+    return `Hola ${nombre} retornado`
 }
+
+saludar ("Mica")
+console.log (saludar("Mica"))
