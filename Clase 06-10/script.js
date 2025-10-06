167880dc111153e7a08d0cc6250a02d2092b7a33
Mikasol7 <micaelasol-l@hotmail.com>
2025-10-06 23:47:44 +0000

    Clase 6 oct

---
+++ Clase 06-10/script.js
@@ -0,0 +1,23 @@
+// const span = document.getElementById("span")
+// const button = document.getElementById("button")
+// const input = document.getElementById("input")
+
+// span.innerText="Lo que sea"
+// button.onclick= function(){
+//     span.innerText="Que apretas"
+//     // span.classList.add("verde")
+//     // span.classList.remove("rojo")
+//     span.classList.toggle("azul") 
+// }
+// input.onchange=(ev)=>{
+//     // console.log(ev)
+//     // console.log(ev.target.value)
+//     span.innetText="HOLA"
+// }
+// input.onkeydown=(ev)=>{
+//     console.log(ev)
+//     console.log(ev.target.value)
+//     span.innetText="HOLAa"
+// }
+
+const celdas= document.getElementsByClassName("celda")
