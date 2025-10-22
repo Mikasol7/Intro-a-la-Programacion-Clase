5fb16e3e2dc913b74e64766e6f6dd70cb9299f1b
Mikasol7 <micaelasol-l@hotmail.com>
2025-10-22 23:47:04 +0000

    Clase 22 oct

--- Solicitudes/script.js
+++ Solicitudes/script.js
@@ -1,18 +1,31 @@
-const obtener= document.getElementById ("obtener")
-const tareas= document.getElementById ("tareas")
+const URL = "https://todo-api-production-1c4d.up.railway.app"
 
-function crearTarjeta (title, completed) {
-    const div= document.createElement("div")
-    div.classList.add("card")
-    div.innerHTML=`<span>${title}</span> <span>${completed ?"SI":"NO"}</span>`
-    return div
+const obtener = document.getElementById("obtener")
+const tareas = document.getElementById("tareas")
+
+function crearTarjeta(title, completed) {
+  const div = document.createElement("div")
+  div.classList.add("card")
+  div.innerHTML = `<span>${title}</span> <span>${completed ? "SI" : "NO"}</span>`
+  return div
 }
 
-fetch('https://jsonplaceholder.typicode.com/todos?userId=1')
-  .then((response) => response.json())
-  .then((json) => {
-    json.forEach(element => {
-        const card= crearTarjeta (element.title, element.completed)
-        tareas.appendChild (card)
+function obtenerTareasPaginadas (page=0,take=5) {
+  fetch(`${URL}/tasks?page=${page}&take=${take}`)
+    .then((response) => response.json())
+    .then((json) => {
+      json.data.forEach(element => {
+        const card = crearTarjeta(element.title, element.completed)
+        tareas.appendChild(card)
+      });
+    const cantPages= Math.ceil(json.total_result/take)
     });
-  });
+}
+
+function crearComponentePaginado (cantPages, page) {
+  for (let i=0; i<cantPages; i++){
+    crearboton (i)
+  }
+}
+
+obtenerTareasPaginadas()
